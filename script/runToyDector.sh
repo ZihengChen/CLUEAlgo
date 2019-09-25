d0f89e4c0bf002f83372d87cc387710bc48b55dd
Ziehng Chen <zava153@live.rhul.ac.uk>
2019-09-25 21:09:24 +0200

    add log for script/run

--- script/runToyDector.sh
+++ script/runToyDector.sh
@@ -4,23 +4,25 @@ export D0=5
 export DELTAC=5
 export RHOC=8
 
+echo "running on CPU"
 bin/main toyDetector_1000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_2000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_3000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_4000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_5000 $DC $D0 $DELTAC $RHOC 0 10 0
-bin/main toyDetector_8000 $DC $D0 $DELTAC $RHOC 0 10 0
+bin/main toyDetector_6000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_7000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_8000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_9000 $DC $D0 $DELTAC $RHOC 0 10 0
 bin/main toyDetector_10000 $DC $D0 $DELTAC $RHOC 0 10 0
 
+echo "running on GPU"
 bin/main toyDetector_1000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_2000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_3000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_4000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_5000 $DC $D0 $DELTAC $RHOC 1 10 0
-bin/main toyDetector_8000 $DC $D0 $DELTAC $RHOC 1 10 0
+bin/main toyDetector_6000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_7000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_8000 $DC $D0 $DELTAC $RHOC 1 10 0
 bin/main toyDetector_9000 $DC $D0 $DELTAC $RHOC 1 10 0
